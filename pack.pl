57eb91338da48fdccbc5a35fe4090cbcf122587e
mgondan <Matthias.Gondan@psy.ku.dk>
2019-10-21 19:16:34 +0200

    Update pack.pl

--- pack.pl
+++ pack.pl
@@ -1,4 +1,4 @@
 name(mathml).
 title('Translates mathematical expressions to compounds that can be used by SWISH html//1.').
-version('0.0.1').
+version('0.0.2').
 author('Matthias Gondan', 'Matthias.Gondan@psy.ku.dk').
