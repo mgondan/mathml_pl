2f26093f5aa4c18f6c0f5be340d3140801097b57
mgondan <Matthias.Gondan@psy.ku.dk>
2019-10-31 14:03:45 +0100

    Update pack.pl

--- pack.pl
+++ pack.pl
@@ -1,4 +1,4 @@
 name(mathml).
 title('Translates mathematical expressions to compounds that can be used by html//1.').
-version('0.0.4').
+version('0.0.6').
 author('Matthias Gondan', 'Matthias.Gondan@psy.ku.dk').
