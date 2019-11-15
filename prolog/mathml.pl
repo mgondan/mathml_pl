e84ec83b42fa00862ef7f7bb6638b246ee035a28
mgondan <Matthias.Gondan@psy.ku.dk>
2019-11-15 16:09:17 +0100

    Update mathml.pl

--- prolog/mathml.pl
+++ prolog/mathml.pl
@@ -47,8 +47,8 @@ example(Flags, A) :-
 %
 % For SWISH
 %
- example(Flags, A) :-
-    mathml(Flags, A, M) -> html(html(math(M))) ; writeln(failed).
+% example(Flags, A) :-
+%    mathml(Flags, A, M) -> html(html(math(M))) ; writeln(failed).
 
 %
 % Helper predicate for flags
