62ae2267b76a52c2584ed6f9b880f052c9ba6db4
mgondan <Matthias.Gondan@psy.ku.dk>
2019-11-16 21:43:19 +0100

    Create rserve.pl

---
+++ prolog/rserve.pl
@@ -0,0 +1,21 @@
+:- <- {|r||
+       overline = identity
+       |}.
+
+:- <- {|r||
+       instead_of = function(instead, of)
+           return(instead)
+       |}.
+
+:- <- {|r||
+       denoting = function(x, y, desc)
+           return(y)
+       |}.
+
+:- <- {|r||
+       frac = `/`
+       |}.
+
+:- <- {|r||
+       dfrac = `/`
+       |}.
