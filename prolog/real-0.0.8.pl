53d1d3c54c2373b91825646e4f6f5d96b40ec0e8
mgondan <Matthias.Gondan@psy.ku.dk>
2019-11-17 11:42:23 +0100

    Update real-0.0.8.pl

--- prolog/real-0.0.8.pl
+++ prolog/real-0.0.8.pl
@@ -3,7 +3,7 @@
 % R interface
 :- instead_of <- (
        function(instead, of) :-
-       return(instead)).
+           return(instead)).
 
 :- frac <- '`/`'.
 
@@ -13,4 +13,9 @@
 
 :- denoting <- (
        function(x, y, desc) :-
-       return(y)).
+           return(y)).
+
+:- format_tratio <- (
+       function(x) :-
+           s = sprintf(ifelse(abs(x) > 10, '%.1f', '%.2f'), x)
+           return(s)).
