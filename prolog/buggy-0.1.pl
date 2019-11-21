ae106a35bb3ec02e1487b97771c2c288ab075d20
mgondan <Matthias.Gondan@psy.ku.dk>
2019-11-21 19:14:56 +0100

    Update and rename buggy.pl to buggy-0.1.pl

--- prolog/buggy-0.1.pl
+++ prolog/buggy-0.1.pl
@@ -1,12 +1,9 @@
-:- use_module(library(real)).
-:- use_module(library(mathml)).
-:- use_module(library(http/html_write)).
-
-:- include('real-0.0.8.pl').
-% :- include('mathml-0.0.8.pl').
-% :- include('rserve-0.0.8.pl').
-
-:- discontiguous buggy/2.
+:- include('mathml-0.0.8.pl').
+:- include('html-0.1.pl').
+:- include('rserve-0.0.8.pl').
+:- discontiguous expert/1.
+:- discontiguous buggy/1.
+:- discontiguous fb/2.
 :- discontiguous intermediate/1.
     
 % Calling interface
@@ -19,15 +16,17 @@ ex :-
     route(TTEST, Sol, Path),
     Res <- Sol,
     mathml(Sol = Res, S), html(html(S)), 
-    mathml(list(';', Path), P), html(html(P)).    
+    mathml(list(';', Path), P), html(html(P)),
+    maplist(fb, Path, B),
+    html(html(ul(B))).
 
 % Correct steps in the calculation
 step(A, X, [expert(A = X)]) :-
     expert(A = X).
 
 % Apply buggy rule
-step(A, X, [buggy(A \= X, Bug)]) :-
-    buggy(A \= X, Bug).
+step(A, X, [buggy(A \= X)]) :-
+    buggy(A \= X).
 
 % Enter terms
 step(A, X, Steps) :-
@@ -108,30 +107,24 @@ item('TTEST'(T0-EOT, T0, EOT, Mu, s, _S_T0, _S_EOT, N)) -->
       X_T0 <- 'T0',
       X_s_T0 <- 'S_T0',
       X_EOT <- 'EOT',
-      X_s_EOT <- 'S_EOT' },
+      X_s_EOT <- 'S_EOT',
+      H = ["HDRS", T0, EOT, T0-EOT],
+      R1 = ["Mean", X_T0,   X_EOT, X_T0_EOT],
+      R2 = ["SD",   X_s_T0, X_s_EOT, X_s],
+      maplist([X, Y] >> (Y = \mml(X)), H, TH),
+      maplist([X, Y] >> (Y = \mml(round1(X))), R1, TR1),
+      maplist([X, Y] >> (Y = \mml(round1(X))), R2, TR2)},
     html([h1(["Paired ", \mml(t), "-test"]),
-        p(["Consider a clinical study on rumination-focused Cognitive ",
-           "Behavioral Therapy (rfCBT) with ", \mml(N = X_N), " ",
-	       "patients."]),
-    table(class("table"),
-          [thead(tr([th(\mml(N = X_N)), th(\mml(T0)),
-	          th(\mml(EOT)), th(\mml(T0-EOT))])),
-           tbody([tr([th("Mean"), td(\mml(X_T0)), td(\mml(X_EOT)),
-                      td(\mml(round1(X_T0_EOT)))]),
-  		          tr([th("SD"), td(\mml(X_s_T0)), td(\mml(X_s_EOT)),
-                      td(\mml(X_s))])])]),
-    p(["Does rfCBT lead to a relevant reduction (i.e., more than ",
-       \mml(Mu = X_mu), " HDRS units) in mean HDRS scores between ",
-       "Baseline (", \mml(T0), ") and End of Treatment ",
-       "(", \mml(EOT), ")? Please determine the ", \mml(t), "-ratio."]),
-    div(class("panel panel-default"), 
-    div(class("panel-body"),
-    div(class("form-group"), 
-     [label("Response"),
-      div(class("input-group"), 
-          [input([class("form-control"), id(response), name(response)]),
-            div(class("input-group-btn"),
-                button([type(submit), id(submit), class("btn btn-default")], "Submit"))])])))]).
+          p(["Consider a clinical study on rumination-focused Cognitive ",
+             "Behavioral Therapy (rfCBT) with ", \mml(N = X_N), " ",
+	         "patients."]),
+          \table(TH, [TR1, TR2]),
+          p(["Does rfCBT lead to a relevant reduction (i.e., more than ",
+             \mml(Mu = X_mu), " HDRS units) in mean HDRS scores between ",
+             "Baseline (", \mml(T0), ") and End of Treatment ",
+             "(", \mml(EOT), ")? Please determine ",
+             "the ", \mml(t), "-ratio."]),
+           \response(response)]).
 
 intermediate('TTEST'(_, _, _, _, _, _, _, _)).
 intermediate(tratio(_, _, _, _)).
@@ -139,18 +132,32 @@ intermediate(tratio(_, _, _, _)).
 expert('TTEST'(X, _T0, _EOT, Mu, S, _S_T0, _S_EOT, N) = 
        tratio(X, Mu, S, N)).
 
+fb(expert('TTEST'(_X, _T0, _EOT, _Mu, _S, _S_T0, _S_EOT, _N) = _), 
+   li(["Correctly identified the problem as a ", 
+      \mml(t), "-test for paired samples."])).
+
 expert(tratio(X, Mu, S, N) = 
        format_tratio(dfrac(X - Mu, S / sqrt(N)))).
 
+fb(expert(tratio(_X, _Mu, _S, _N) = _), 
+   li(["Correctly applied the ", \mml(t), "-ratio."])).
+
 % Wrong X
-buggy(TTEST \= TRATIO, BUG) :-
+buggy(TTEST \= TRATIO) :-
     TTEST = 'TTEST'(D, T0, _EOT, Mu, S, _S_T0, _S_EOT, N),
     TRATIO = tratio(W, Mu, S, N),
-    BUG = bug(data, instead_of('T0', 'T0'-'EOT')),
     W = denoting(overline('D'), instead_of(T0, D), 
         "the average change between baseline and end of treatment").
 
+fb(buggy(TTEST \= _), FB) :-
+    TTEST = 'TTEST'(D, T0, _EOT, _Mu, _S, _S_T0, _S_EOT, _N),
+    FB = li(["Wrong data: ", \mml(T0), " was used instead of ", \mml(D)]).
+
 % Omit implicit parenthesis
-buggy(dfrac(A - B, C / D) \= A - dfrac(B, C) / D, Bug) :-
-    Bug = bug(calc, dfrac(red(paren(black('A' - 'B'))), 
-                         red(paren(black('C' / 'D'))))).
+buggy(dfrac(A - B, C / D) \= A - dfrac(B, C) / D).
+
+fb(buggy(dfrac(A - B, C / D) \= A - dfrac(B, C) / D), FB) :-
+   FB = li(["Please do not omit the implicit parentheses ",
+            "in the fraction ", \mml(dfrac(red(paren(black('A' - 'B'))), 
+                         red(paren(black('C' / 'D')))))]).
+ 
