37f2d6d4b2fdb52b05ccdc520520bb8f76ecba30
mgondan <Matthias.Gondan@psy.ku.dk>
2019-11-22 23:48:37 +0100

    Update input-0.1.pl

--- prolog/input-0.1.pl
+++ prolog/input-0.1.pl
@@ -5,6 +5,10 @@
 %
 % 1.5 cm
 %
+string_number(Number, String, Options) :-
+    string_codes(String, Codes), 
+    number(Number, Options, Codes, []).
+
 number(Number, Options) --> 
     numb(N, NumOpt),
     blanks,
@@ -19,25 +23,32 @@ number(Number, Options) -->
 % Real number
 numb(Number, Options) -->
     sgn(S, SgnOpt),
-    int(I),
+    int(I, IntOpt),
     dotfrac(F, DotOpt),
+    { \+ option(int(empty), IntOpt); 
+      \+ option(dec(0), DotOpt),
+      merge_options(IntOpt, DotOpt, IntDotOpt)
+    },
     power(E),
     { Number is S * (I + F) * 10^E, 
       option(dec(D), DotOpt, 0),
       Dec is D - E,
-      merge_options([dec(Dec) | SgnOpt], DotOpt, Options) }.
+      merge_options([dec(Dec) | SgnOpt], IntDotOpt, Options) 
+    }.
 
 % Sign
 sgn(+1, [sgn(+)]) --> "+".
 sgn(-1, [sgn(-)]) --> "-"; [226, 136, 146].
 sgn( 1, []) --> "".
 
-int(I) --> 
+int(I, []) --> 
     digits([H | T]),
     {number_codes(N, [H | T]), I is N + 0.0}.
 
+int(0, [int(empty)]) --> "".
+
 dotfrac(F, Options) --> dot, frac(F, Options).
-dotfrac(0, []) --> [].
+dotfrac(0, [dot(''), dec(0)]) --> [].
 
 dot --> ".".
 dot --> ",".
@@ -46,8 +57,9 @@ frac(F, [dec(Dec)]) --> digits([H | T]),
     {number_codes(N, [H | T]), length([H | T], Dec), F is N/10.0^Dec}.
 
 % times 10^E
-power(E) --> exp_e, sgn(S, _), int(C),
-    {E is S * C}.
+power(E) --> exp_e, sgn(S, _), int(C, IntOpt),
+    {\+ option(int(empty), IntOpt),
+      E is S * C}.
 power(0) --> [].
 
 exp_e --> "E".
@@ -76,53 +88,51 @@ si(g) --> "g".
 si(m) --> "m".
 
 ex :-
-    C = `.5 kg`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
+    S = ".5 kg", writeln(S),
+    string_number(N, S, Options),
     writeln(N), writeln(Options).
 
 ex :-
-    C = `1.5 kg`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
+    S = "1.5 kg", writeln(S),
+    string_number(N, S, Options),
     writeln(N), writeln(Options).
 
 ex :-
-    C = `15 kg`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
+    S = "15 kg", writeln(S),
+    string_number(N, S, Options),
     writeln(N), writeln(Options).
 
 ex :-
-    C = `15 g`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
+    S = "15 g", writeln(S),
+    string_number(N, S, Options),
     writeln(N), writeln(Options).
 
 ex :-
-    C = `-15 g`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
+    S = "-15 g", writeln(S),
+    string_number(N, S, Options),
     writeln(N), writeln(Options).
 
 ex :-
-    C = `-15E10 g`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
+    S = "-15E10 g", writeln(S),
+    string_number(N, S, Options),
     writeln(N), writeln(Options).
 
 ex :-
-    C = `-1.5E-10 g`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
-    writeln(N), writeln(Options).
-
-
-ex :-
-    C = `0.09`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
+    S = "-1.5E-10 g", writeln(S),
+    string_number(N, S, Options),
     writeln(N), writeln(Options).
 
 ex :-
-    C = `9%`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
+    S = "0.09", writeln(S),
+    string_number(N, S, Options),
     writeln(N), writeln(Options).
 
 ex :-
-    C = `9.1 %`, string_codes(S, C), writeln(S),
-    number(N, Options, C, []),
+    S = "9%", writeln(S),
+    string_number(N, S, Options),
     writeln(N), writeln(Options).
 
+ex :-
+    S = "9.1 %", writeln(S),
+    string_number(N, S, Options),
+    writeln(N), writeln(Options).
