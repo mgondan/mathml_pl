85d9cf3825c860cd7b822d58532197b5917a7092
Matthias Gondan <Matthias.Gondan@psy.ku.dk>
2019-12-21 20:15:07 +0100

    New version 0.1

--- pack.pl
+++ pack.pl
@@ -1,4 +1,4 @@
 name(mathml).
 title('Translates mathematical expressions to compounds that can be used by html//1 (SWISH) or html//3 (normal SWIPL, with pack http/html_write).').
-version('0.0.8').
+version('0.1').
 author('Matthias Gondan', 'Matthias.Gondan@psy.ku.dk').
