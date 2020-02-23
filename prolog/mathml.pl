e6a58c86a3274997b82bbd9e53b19c35c528032a
mgondan <Matthias.Gondan@psy.ku.dk>
2020-02-23 15:06:18 +0100

    Update mathml.pl

--- prolog/mathml.pl
+++ prolog/mathml.pl
@@ -1480,7 +1480,7 @@ math(_, ancova_ff(_, _Primary, _Covariates, _Strata, Therapy, _Other), sub('F', 
 math(_, ancova_fff(_, _Primary, _Covariates, _Strata, Therapy, _Other), sub('F', Therapy)).
 math(_, ancova_ffff(_, _Primary, _Covariates, _Strata, Therapy), sub('F', Therapy)).
 
-math(_, Tilde, Dependent ~ list(+, Predictors)) :-
+math(_, Tilde, Dependent ~ [Predictors]) :-
     compound(Tilde),
     compound_name_arguments(Tilde, tilde, [Dependent | Predictors]).
 
