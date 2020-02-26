5c2084645b8d72ba730e93a36bb31518e1a33914
mgondan <Matthias.Gondan@psy.ku.dk>
2020-02-26 12:25:33 +0100

    Update mathml.pl

--- prolog/mathml.pl
+++ prolog/mathml.pl
@@ -1495,13 +1495,13 @@ math(Flags, anova_f(_, Therapy), Flags, sub('F', Therapy)).
 math(Flags, 'TTEST'(D, T0, EOT, Mu, S, S_T0, S_EOT, N),
      Flags, fun('TTEST', (D, T0, EOT, Mu, S, S_T0, S_EOT, N))).
 
-math(Flags, tratio(X, Mu, S, N), Flags, fun("paired t-test", (X, S; Mu, N))).
+math(Flags, tratio(X, Mu, S, N), Flags, fun("paired t-test", [X, S; Mu, N])).
 
 math(Flags, tratio_groups(M_A, S_A, N_A, M_B, S_B, N_B, Mu, _Tails, _Alpha),
      Flags, fun("independent t-test", [M_A, S_A, M_B, S_B; Mu, N_A, N_B])).
 
 math(Flags, tratio_paired(D, _T0, _EOT, Mu, S_D, _S_T0, _S_EOT, N, _Tails, _Alpha),
-     Flags, fun("paired t-test", (D, S_D; Mu, N))).
+     Flags, fun("paired t-test", [D, S_D; Mu, N])).
 
 math(Flags, var_pool(V_A, N, V_B, N), Flags, dfrac(V_A + V_B, 2)).
 
