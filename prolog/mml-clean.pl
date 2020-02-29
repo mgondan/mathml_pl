05460e238e4d5041230a9e59b0db3d9063fefee2
mgondan <Matthias.Gondan@psy.ku.dk>
2020-02-29 17:28:17 +0100

    Create mml-clean.pl

---
+++ prolog/mml-clean.pl
@@ -0,0 +1,182 @@
+:- module(mathml,
+    [ pl2m/2 ]).
+
+:- use_module(library(real)).
+:- use_module(library(http/html_write)).
+
+:- discontiguous pl2m/2.
+:- discontiguous precedence/2.
+:- discontiguous example/0.
+
+% Punctuation
+pl2m(punct('_'), &(nbsp)).
+pl2m(punct(' '), mspace(width(thickmathspace), [])).
+pl2m(punct(ldots), mi(&(hellip))).
+pl2m(punct(cdots), mi(&(ctdot))).
+
+example :-
+    example(punct('_')).
+
+% Operator signs
+pl2m(op(+), mo(+)).
+pl2m(op(+), mo(+)).
+pl2m(op(-), mo(-)).
+pl2m(op(*), mo(&(sdot))).
+pl2m(op(/), mo(/)).
+pl2m(op(=\=), mo(&('#8203'))).
+pl2m(op(=), mo(=)).
+pl2m(op(<), mo(&(lt))).
+pl2m(op(>), mo(&(gt))).
+pl2m(op(=<), mo(&(le))).
+pl2m(op(>=), mo(&(ge))).
+pl2m(op(\=), mo(&(ne))).
+pl2m(op(!), mo(!)).
+pl2m(op('%'), mo('%')).
+pl2m(op(','), mo(&(comma))).
+pl2m(op(';'), mo(&('#59'))).
+pl2m(op('|'), mo('|')).
+pl2m(op(invisible_times), mo(&('#x2062'))).
+pl2m(op(->), mo(&(rArr))).
+pl2m(op(~>), mo(&(zigrarr))).
+pl2m(op(~), mo(~)).
+
+example :-
+    example(op(invisible_times)).
+
+% Linear model
+pl2m(linear(Dep, Icpt, Cov, Strata, Main, Other, _Data), M) :-
+    append([Icpt, Cov, Strata, Main, Other], Predictors),
+    pl2m(operator(op(~), Dep, list(+, Predictors)), M).
+
+% Lists
+pl2m(list(+, []), M) :-
+    pl2m(0, M).
+
+pl2m(list(+, [A]), M) :-
+    pl2m(A, M).
+
+pl2m(list(+, [H1, H2 | T]), M) :-
+    pl2m(operator(op(+), H1, list(+, [H2 | T])), M).
+
+precedence(list(_, []), 0).
+
+precedence(list(+, [_ | _]), Prec) :-
+    precedence(op(+), yfx, Prec).
+
+% Binary operators
+pl2m(operator(Op, Left, Right), mrow([L, F, R])) :-
+    pl2m(Op, F),
+    precedence(Op, xfx, Prec),
+    precedence(Left, LPrec),
+    (   LPrec >= Prec
+    ->  pl2m(paren(Left), L)
+    ;   pl2m(Left, L)),
+    precedence(Right, RPrec),
+    (   RPrec >= Prec
+    ->  pl2m(paren(Right), R)
+    ;   pl2m(Right, R)).
+
+pl2m(operator(Op, Left, Right), mrow([L, F, R])) :-
+    pl2m(Op, F),
+    precedence(Op, yfx, Prec),
+    precedence(Left, LPrec),
+    (   LPrec > Prec
+    ->  pl2m(paren(Left), L)
+    ;   pl2m(Left, L)),
+    precedence(Right, RPrec),
+    (   RPrec >= Prec
+    ->  pl2m(paren(Right), R)
+    ;   pl2m(Right, R)).
+
+pl2m(operator(Op, Left, Right), mrow([L, F, R])) :-
+    pl2m(Op, F),
+    precedence(Op, xfy, Prec),
+    precedence(Left, LPrec),
+    (   LPrec >= Prec
+    ->  pl2m(paren(Left), L)
+    ;   pl2m(Left, L)),
+    precedence(Right, RPrec),
+    (   RPrec > Prec
+    ->  pl2m(paren(Right), R)
+    ;   pl2m(Right, R)).
+
+% Parentheses
+pl2m(parentheses(A), mrow([mo('('), M, mo(')')])) :-
+    pl2m(A, M).
+
+pl2m(bracket(A), mrow([mo('['), M, mo(']')])) :-
+    pl2m(A, M).
+
+pl2m(curly(A), mrow([mo('{'), M, mo('}')])) :-
+    pl2m(A, M).
+
+pl2m(abs(A), mrow([mo('|'), M, mo('|')])) :-
+    pl2m(A, M).
+
+% Determine level of parentheses
+pl2m(paren(A), M) :-
+    paren(A, 0),
+    pl2m(parentheses(A), M).
+
+pl2m(paren(A), M) :-
+    paren(A, 1),
+    pl2m(bracket(A), M).
+
+pl2m(paren(A), M) :-
+    paren(A, 2),
+    pl2m(curly(A), M).
+
+pl2m(paren(A), M) :-
+    paren(A, P),
+    P > 2,
+    pl2m(parentheses(A), M).
+
+% Symbols and variables
+pl2m(dependent(A), mi(A)).
+pl2m(integer(A), mn(A)).
+pl2m(intercept(A), M) :-
+    pl2m(integer(A), M).
+pl2m(stratum(A), mi(A)).
+pl2m(covariate(A), mi(A)).
+pl2m(predictor(A), mi(A)).
+pl2m(main(A), mi(A)).
+
+% Operator precedence
+precedence(operator(Op, _L, _R), Prec) :-
+    precedence(Op, yfx, Prec).
+
+precedence(operator(Op, _L, _R), Prec) :-
+    precedence(Op, xfy, Prec).
+
+precedence(operator(Op, _L, _R), Prec) :-
+    precedence(Op, xfx, Prec).
+
+precedence(op(Op), Fix, Prec) :-
+    current_op(Prec, Fix, Op).
+
+precedence(dependent(_), 0).
+precedence(integer(_), 0).
+precedence(intercept(_), 0).
+precedence(stratum(_), 0).
+precedence(covariate(_), 0).
+precedence(predictor(_), 0).
+precedence(main(_), 0).
+
+% todo
+paren(_, 0).
+
+
+example :-
+    pl2m(linear(dependent(y),
+                [intercept(1)],
+                [covariate(t0)],
+                [stratum(sex), stratum(center)],
+                [main(therapy)],
+                [],
+                data(d)), M),
+    writeln(M).
+
+example(P) :-
+    pl2m(P, M),
+    html(M, Out, []),
+    print_html(Out).
