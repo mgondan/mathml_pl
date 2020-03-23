a3ac77497db30b8c5ae4c187568c0fe6d6216617
mgondan <Matthias.Gondan@psy.ku.dk>
2020-03-23 09:35:27 +0100

    Update mathml.pl

--- prolog/mathml.pl
+++ prolog/mathml.pl
@@ -1413,9 +1413,7 @@ math(Flags, ancova_ffff(_, _Primary, _Covariates, _Strata, _Other, _Interactions
 math(Flags, ancova_fffff(_, _Primary, _Covariates, _Strata, _Other, _Interactions, _Exclude, Therapy), Flags, sub(atom('F'), Therapy)).
 math(Flags, ancova_ffffff(_, _Primary, _Covariates, _Strata, _Other, _Interactions, _Exclude, Therapy), Flags, sub(atom('F'), Therapy)).
 denot(_, ancova_ffffff(_, _Primary, _Covariates, _Strata, _Other, _Interactions, _Exclude, Therapy), 
-    denoting(m, 'the model')).
-
-
+    [denoting(m, 'the model')]).
 
 ml(Flags, Tilde, M) :-
     compound(Tilde),
