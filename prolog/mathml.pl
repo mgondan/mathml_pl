799aef3be42db285fba0cdfa51a95da6005bb8e9
mgondan <Matthias.Gondan@psy.ku.dk>
2020-05-06 17:45:51 +0200

    Space between symbol and operator in omit_right

--- prolog/mathml.pl
+++ prolog/mathml.pl
@@ -769,12 +769,12 @@ ml(Flags, omit_right(Err, Expr), M) :-
 ml(Flags, omit_right(Err, Expr), M) :-
     show(Flags, Err),
     compound_name_arguments(Expr, Op, [L, R]),
-    ml(Flags, (L, cancel(Err, (Op, ' ', R))), M).
+    ml(Flags, (L, ' ', cancel(Err, (Op, ' ', R))), M).
 
 ml(Flags, omit_right(Err, Expr), M) :-
     fix(Flags, Err),
     compound_name_arguments(Expr, Op, [L, R]),
-    ml(Flags, (L, color(Err, (Op, ' ', R))), M).
+    ml(Flags, (L, ' ', color(Err, (Op, ' ', R))), M).
 
 ml(Flags, omit_right(Err, Expr), M) :-
     correct(Flags, Err),
