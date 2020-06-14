7e493ee726939526af9b9122d8c6528060def3a1
mgondan <Matthias.Gondan@psy.ku.dk>
2020-06-14 19:39:28 +0200

    Update mathml.pl

--- prolog/mathml.pl
+++ prolog/mathml.pl
@@ -1678,7 +1678,8 @@ math(Flags, upper(Alpha), Flags, 1 - Alpha).
 math(Flags, uqbinom("upper", "dist", Alpha, N, P), Flags, argmin(c, ubinom(c, N, P) =< Alpha)).
 math(Flags, uqbinom("lower", "dist", Alpha, N, P), Flags, argmax(c, pbinom(c, N, P) =< Alpha)).
 math(Flags, instead_of(Code, uqbinom("lower", "dist", Alpha, N, P), _, uqbinom("upper", "dist", Alpha, N, P), _), 
-        Flags, fun(under(instead_of(Code, "argmax", "argmin"), c), instead_of(Code, pbinom(c, N, P), ubinom(c, N, P))) =< Alpha).
+        Flags, fun(instead_of(Code, under("argmax", c), under("argmin", c)), 
+                   instead_of(Code, pbinom(c, N, P), ubinom(c, N, P))) =< Alpha).
         
 math(Flags, uqbinom("upper", "density", Alpha, N, P), Flags, argmin(c, dbinom(c, N, P) =< Alpha)).
 math(Flags, uqbinom("lower", "density", Alpha, N, P), Flags, argmax(c, dbinom(c, N, P) =< Alpha)).
